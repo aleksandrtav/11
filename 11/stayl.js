cc133c7db3f710e1407338343621fd128620ab32
Aleksandr Turbarov <107515557+aleksandrtav@users.noreply.github.com>
2022-07-19 21:54:34 +0300

    Add files via upload

---
+++ 11/stayl.js
@@ -0,0 +1,16 @@
+
+function getPeople(handshake) {
+    for (let employees = 0; employees < handshake; employees++){
+       return employees += handshake;
+        
+    }
+    
+    
+}
+console.log( getPeople(1) ); // 1
+console.log( getPeople(3) ); // 2
+console.log( getPeople(6) ); // 3
+console.log( getPeople(10) ); // 4
+console.log( getPeople(15) ); // 5
+console.log(getPeople(120)); // ?
+console.log() 
